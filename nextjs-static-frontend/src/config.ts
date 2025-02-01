e808001161088d570345259e1f2ef87e741dcaee
AlwaysHungrie <dhairyashah98@gmail.com>
2025-02-01 09:01:39 +0530

    mobile rpc fix

--- nextjs-static-frontend/src/config.ts
+++ nextjs-static-frontend/src/config.ts
@@ -1,67 +1,34 @@
+import { base, scroll, mainnet } from 'viem/chains'
 
 const BASE = {
-  id: 8453,
-  name: 'Base',
-  nativeCurrency: {
-    name: 'Base',
-    symbol: 'ETH',
-    decimals: 18,
-  },
+  ...base,
   rpcUrls: {
-    default: {
+    ...base.rpcUrls,
+    privyWalletOverride: {
       http: [process.env.BASE_RPC_URL!],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Base Explorer',
-      url: 'https://basescan.org',
-    },
-  },
-}
-
-const SCROLL = {
-  id: 534352,
-  name: 'Scroll',
-  nativeCurrency: {
-    name: 'Scroll',
-    symbol: 'ETH',
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: [process.env.SCROLL_RPC_URL!],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Scroll Explorer',
-      url: 'https://scrollscan.com',
     },
   },
 }
 
 const ETHEREUM = {
-  id: 1,
-  name: 'Ethereum',
-  nativeCurrency: {
-    name: 'Ethereum',
-    symbol: 'ETH',
-    decimals: 18,
-  },
+  ...mainnet,
   rpcUrls: {
-    default: {
+    ...mainnet.rpcUrls,
+    privyWalletOverride: {
       http: [process.env.ETHEREUM_RPC_URL!],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Etherscan',
-      url: 'https://etherscan.io',
     },
   },
 }
 
+const SCROLL = {
+  ...scroll,
+  rpcUrls: {
+    ...scroll.rpcUrls,
+    privyWalletOverride: {
+      http: [process.env.SCROLL_RPC_URL!],
+    },
+  },
+}
 
 export const CONFIG = {
   TELEGRAM_URL: 'https://t.me/getduinbot',
